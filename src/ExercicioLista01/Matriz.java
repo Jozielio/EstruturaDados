8b363bff3a1c48a9b6acdbb37684798e84fb41ad
Jozielio <jozieliosantiago@gmail.com>
2016-03-04 20:08:15 -0300

    Estrutura

--- src/ExercicioLista01/Matriz.java
+++ src/ExercicioLista01/Matriz.java
@@ -11,15 +11,3 @@ abstract class Matriz {
 	static int matrizResultado[][] = new int[matriz1.length][matriz2[0].length];
 	
 }
-//
-////TODO pesquisar outras formas
-////for(int i = 0; i < matrizResultado.length;i++){
-//int linha = 0;
-//	for(int j = 0; j < matrizResultado[linha].length;j++){
-//		for(int k = 0; k < matriz1[0].length; k++){
-//			matrizResultado[linha][j] += (matriz1[linha][k] * matriz2[k][j]);
-//		}
-//		linha++;
-//	}
-////}
-//
